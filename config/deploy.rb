92f25a7d75b1a2cdeffbb133d22f9e456d79e25d
Luke Closs <gravatar@5thplane.com>
2009-02-17 05:15:00 -0500

    add capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "VanCalagator"
+set :repository,  "git@github.com:lukec/calagator.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "cal.wikrad.com"
+role :web, "cal.wikrad.com"
+role :db,  "cal.wikrad.com", :primary => true
