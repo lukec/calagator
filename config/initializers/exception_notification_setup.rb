b09492727d11b46cd5f4ee6790888609e1a714a2
Luke Closs <gravatar@5thplane.com>
2009-03-16 21:39:05 -0700

    update the notification email address

--- config/initializers/exception_notification_setup.rb
+++ config/initializers/exception_notification_setup.rb
@@ -2,5 +2,5 @@
 # trivial obfuscation to limit the number of robots that get our email
 # addresses. To generate this string, use the result of a command like:
 #   ['you@host.ext'].pack('u')
-ExceptionNotifier.exception_recipients = "<:6=A;\"ME<G)O<D!P<F%G;6%T:6-R869T+F-O;0``\n".unpack("u")
-ExceptionNotifier.email_prefix = "[ERROR calagator] "
+ExceptionNotifier.exception_recipients = "3=F%N8V%L0#5T:'!L86YE+F-O;0``\n".unpack("u")
+ExceptionNotifier.email_prefix = "[ERROR vancal] "
